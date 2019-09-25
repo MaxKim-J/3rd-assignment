155bc1e82c525c1b0b52d02fbc962b4a2fdc821a
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-25 13:57:13 +0900

    Fetch api로 Movie list를 Json형식으로 가져오기

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-window.onload = async () => {
-	const app = document.querySelector("#app");
-	const loading = document.createElement("p");
-	loading.innerText = "Loading...";
-	app.appendChild(loading);
-	const main = async () => {
-		const {
-			data: {
-				data: { movie: data }
-			}
-		} = await axios.get(
-			"https://yts.lt/api/v2/movie_details.json?movie_id=2019"
-		);
+const app = document.querySelector("#app");
 
-		console.log(data);
-		const title = document.createElement("p");
-		const img = document.createElement("img");
-		const desc = document.createElement("p");
-		title.innerText = data.title_long;
-		img.src = data.medium_cover_image;
-		desc.innerText = data.description_full;
-		const card = document.createElement("p");
-		card.append(img, title, desc);
-		app.removeChild(loading);
-		app.appendChild(card);
-	};
+function getMovieList() {
+    fetch("https://yts.lt/api/v2/list_movies.json?sort_by=download_count"
+    ).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+		const movies = json.data.movies
+		componentMake(movies);
+	})
+}
 
-	await main();
+function componentMake(arr) {
+	// Json 데이터를 넣는 컴포넌트 만들기
+	// genre, title, summary
+	console.log(arr)
+}
+
+function init() {
+	getMovieList();
 };
+
+init();
