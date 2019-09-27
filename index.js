ab8d9d1961bb306910577b7984f59c4f9769122d
Max Jonghyuk Kim <hwaseen@gmail.com>
2019-09-28 02:42:38 +0900

    모달 element위에 상세 데이터 넣기 간단하게 구현

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 const app = document.querySelector("#app");
 const loading = document.querySelector(".loading");
-const modal = document.querySelector(".modal")
-const modalContent = document.querySelector(".modal__content")
-const modalCancelBtn = document.querySelector(".modal__content > button")
+
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
 //todo 모달 이용하여 상세 페이지 만들기(토요일)
 //todo githubpages 배포
 
 function getMovieList() {
   // axios와 JSON 메소드로 꼭 필요한 데이터만 파싱
-  axios.get('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
-    .then(function (response) {
-      const jsonString = JSON.stringify(response.data.data.movies,
-        ["title", "large_cover_image", "summary", "genres"]);
+  axios
+    .get("https://yts.lt/api/v2/list_movies.json?sort_by=download_count")
+    .then(function(response) {
+      console.log(response);
+      const jsonString = JSON.stringify(response.data.data.movies, [
+        "title",
+        "large_cover_image",
+        "summary",
+        "genres"
+      ]);
       const movies = JSON.parse(jsonString);
       componentMake(movies);
+      modalPopUp(movies);
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.log(error);
-    })
+    });
 }
 
-function handleDetailBtn() { 
-  modal.style.display = "block"
-  const btnVal = document.querySelector(".modal")
-  
-  console.log(modalIndex);
+function modalPopUp(movies) {
+  const col1 = document.querySelector(".modal__column1");
+  const col2 = document.querySelector(".modal__column2");
+  const modalCancelBtn = document.querySelector(".cancelBtn");
+  const buttons = document.querySelectorAll(".modalbtn");
+  const modalImg = document.createElement("img");
+  const modalTitle = document.createElement("div");
+  const modalSummary = document.createElement("div");
+
+  buttons.forEach(function(button) {
+    button.addEventListener("click", function(e) {
+      modal.style.display = "block";
+      const indexNum = e.currentTarget.value;
+
+      col1.appendChild(modalImg);
+      col2.appendChild(modalTitle);
+      col2.appendChild(modalSummary);
+
+      modalImg.src = movies[indexNum].large_cover_image;
+      modalTitle.innerText = movies[indexNum].title;
+      modalSummary.innerText = movies[indexNum].summary;
+    });
+  });
+
+  modalCancelBtn.addEventListener("click", function () {
+      modal.style.display = "none";
+      col1.removeChild(modalImg);
+      col2.removeChild(modalTitle);
+      col2.removeChild(modalSummary);
+  });
 }
 
 function componentMake(movies) {
   console.log(movies);
   for (let movieIndex = 0; movieIndex < movies.length; movieIndex++) {
-
     // element 만들기
     const movieDiv = document.createElement("div");
     const imgDiv = document.createElement("div");
@@ -42,7 +73,7 @@ function componentMake(movies) {
     const genreDiv = document.createElement("div");
     const btnDiv = document.createElement("div");
     const detailBtn = document.createElement("button");
-  
+
     // element 자식으로 넣기
     app.appendChild(movieDiv);
     movieDiv.appendChild(imgDiv);
@@ -58,7 +89,7 @@ function componentMake(movies) {
 
     // 포스터, 제목, 요약 가져와서 img, div에 넣기
     img.src = movies[movieIndex].large_cover_image;
-    title.innerText = movies[movieIndex].title;
+    title.innerHTML = `title:\n ${movies[movieIndex].title}`;
     summary.innerText = movies[movieIndex].summary;
 
     // 장르 배열 가져와서 span에 넣기
@@ -71,9 +102,9 @@ function componentMake(movies) {
     }
 
     // button에 텍스트 넣기
-    detailBtn.innerText = "Show Detail"
-    detailBtn.id = `${movieIndex}`
-    detailBtn.classList.add("modalbtn")
+    detailBtn.innerText = "Show Detail";
+    detailBtn.value = `${movieIndex}`;
+    detailBtn.classList.add("modalbtn");
 
     // css클래스 추가
     movieDiv.classList.add("movieDiv");
@@ -81,13 +112,7 @@ function componentMake(movies) {
     imgDiv.classList.add("imgDiv");
 
     // 로딩 메시지 없애기
-    loading.style.display = "none"
-
-    // 디테일 버튼 이벤트 리스너
-    detailBtn.addEventListener("click", handleDetailBtn);
-    modalCancelBtn.addEventListener("click", function () {
-    modal.style.display = "none"
-    });
+    loading.style.display = "none";
   }
 }
 
@@ -96,7 +121,7 @@ function init() {
   //로딩 메시지 만들기
   window.onload = function() {
     loading.innerText = "Loading...";
-    loading.style.display = "block"
+    loading.style.display = "block";
   };
 }
 
